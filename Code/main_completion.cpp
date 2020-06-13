f4139891ca2af10603ea4e53752897708494bede
Alek-Bel <66304481+Alek-Bel@users.noreply.github.com>
2020-06-14 00:25:12 +1200

    main() function for completion
    Just calls robotMovement.doCompletion() instead of .followLine()

---
+++ Code/main_completion.cpp
@@ -0,0 +1,25 @@
+#include "robot.hpp"
+#include <algorithm>
+#include "RobotView.hpp"
+#include "RobotMovement.hpp"
+
+int main(){
+    cameraView.width = 150;
+    cameraView.height = 100;
+    cameraView.data = new char[cameraView.width * cameraView.height*3];
+    cameraView.n_bytes = cameraView.width * cameraView.height*3;
+    RobotMovement robotMovement{};
+
+    if (connectNetwork()!=0){
+        return -1;
+    }
+    std::string fileName = "i0.ppm";
+    usleep(3000000);
+    while(true){
+        takePicture();
+        SavePPMFile(fileName,cameraView);
+        OpenPPMFile(fileName, cameraView);
+        robotMovement.doCompletion();
+        usleep(10000);
+    } //while
+} // main
