2af7c113ab9103264342f3693bae99cf2ec020cb
Alek-Bel <66304481+Alek-Bel@users.noreply.github.com>
2020-06-16 18:28:17 +1200

    Fixed completion and added challenge
    -Completion should be fixed now (robot should choose the right side when it can).
    -Added doChallenge() and doCore()
    -Added doTask(int) which will call the appropriate do function for the given parameter
    -initialise() method (for challenge when the robot should go forward at the start)
    -turnCheck() method for the challenge, which will detect whether or not the robot should turn
    -moveDistance() moves the pixel distance given

--- Code/RobotMovement.hpp
+++ Code/RobotMovement.hpp
@@ -1,42 +1,112 @@
-//#define M_PI 3.14159265359
+
 #include <math.h>
 class RobotMovement{
 public:
+    void doTask(int task);
+private:
     void followLine();
     void regulateMotor();
     void rotate(const double& degrees);
+    void doCore();
     void doCompletion();
+    void doChallenge();
     void goTo(int distanceAhead);
-private:
+    void turnCheck();
     void turnLeft();
     void turnRight();
+    void moveDistance(double distance);
+    void initialise();
     double direction = 0.0; //in degrees;
     const double WHEEL_RADIUS = 8.0;
     const double ROBOT_WIDTH = 30.0;
-    double robotX = 10.0; //robot centre x-coordinate, initial value 10.0
-    double robotY = 100.0; //robot centre y-coordinate, initial value 100.0
     double speed = 20.0; //total speed scalar
+    bool initialised = false;
+    void turnAroundWall();
 };
+void RobotMovement::doTask(int task){
+    if (task == 1)
+        doCore();
+    else if (task == 2)
+        doCompletion();
+    else if (task == 3)
+        doChallenge();
+    else
+        std::cout<<"Task Not Recognised"<<std::endl;
+}
+void RobotMovement::doChallenge(){
+    if (!initialised)
+        initialise();
+    else
+        turnCheck();
+}
+void RobotMovement::initialise(){
+    goTo(160); //moves 300 pixels
+    initialised = true;
+}
 
+void RobotMovement::turnCheck(){
+    std::vector<Pixel> bottomRow = RobotView::getRow(cameraView.height-1);
+    std::vector<Pixel> bottomRowLeft;
+    std::copy_if(bottomRow.begin(),bottomRow.end(),std::back_inserter(bottomRowLeft),
+            [](Pixel p){return p.column < cameraView.width/2;});
+
+    Pixel bottomRedPixel = bottomRow[cameraView.width/2];
+
+    if (bottomRedPixel.isRed()){
+        moveDistance(10);
+        setMotors(0,0);
+        turnRight();
+        setMotors(speed,speed);
+    }
+    else if (!RobotView::hasRedPixels(bottomRowLeft)){
+        turnAroundWall();
+    }
+    else{
+        setMotors(speed,speed);
+    }
+
+}
+
+void RobotMovement::turnAroundWall(){
+    moveDistance(80);
+    turnLeft();
+    moveDistance(80);
+    turnLeft();
+}
+void RobotMovement::moveDistance(double distance){
+    double motorSpeed = speed;
+    double pixelSpeed = (motorSpeed*M_PI*WHEEL_RADIUS)/180.0;
+    while (distance > 0){
+        if (distance - pixelSpeed < 0) {
+            motorSpeed = (distance * 180.0) / (M_PI * WHEEL_RADIUS);
+            pixelSpeed = (motorSpeed*M_PI*WHEEL_RADIUS)/180.0;
+        }
+        setMotors(motorSpeed,motorSpeed);
+        setMotors(0,0);
+        distance-=pixelSpeed;
+        usleep(10000);
+    }
+}
+void RobotMovement::doCore(){
+    followLine();
+}
 void RobotMovement::doCompletion() {
     std::vector<Pixel> leftEdge = RobotView::getLeftEdge();
     std::vector<Pixel> rightEdge = RobotView::getRightEdge();
     if (RobotView::hasWhitePath(leftEdge) || RobotView::hasWhitePath(rightEdge)){
         Pixel averageWhitePixel;
-        bool turnLeft = RobotView::hasWhitePath(leftEdge);
-        if (turnLeft){
-            averageWhitePixel = RobotView::averagePixel(RobotView::getWhitePixels(leftEdge));
-            turnLeft=true;
-        }else{
+        bool turnRight = RobotView::hasWhitePath(rightEdge);
+        if (turnRight){
             averageWhitePixel = RobotView::averagePixel(RobotView::getWhitePixels(rightEdge));
-            turnLeft = false;
+        }else{
+            averageWhitePixel = RobotView::averagePixel(RobotView::getWhitePixels(leftEdge));
         }
         int distanceAhead = RobotView::rowsBetweenPixel(averageWhitePixel);
         goTo(distanceAhead);
-        if (turnLeft)
-            this->turnLeft();
-        else
+        if (turnRight)
             this->turnRight();
+        else
+            this->turnLeft();
     }
     else
         followLine();
@@ -46,6 +116,7 @@ void RobotMovement::goTo(int distance) {
     setMotors(motorSpeed,motorSpeed);
     setMotors(0,0);
 }
+
 void RobotMovement::followLine(){
     regulateMotor();
 }
@@ -64,7 +135,7 @@ void RobotMovement::rotate(const double& degrees){
 }
 
 void RobotMovement::regulateMotor(){
-    const double kp = 0.4; //constant coefficient for error
+    const double kp = 0.3; //constant coefficient for error
     double error;
     int bottomRowIndex = cameraView.height-1;
     std::vector<Pixel> bottomRow = RobotView::getRow(bottomRowIndex);
@@ -76,7 +147,6 @@ void RobotMovement::regulateMotor(){
         int middleColumn = (int)(cameraView.width/2.0);
         error = averageWhitePixel.column-middleColumn;
     }
-    std::cout<<"Error: " << error <<std::endl;
     double speedDifference = kp*error;
     double rightMotorSpeed = speed + speedDifference;
     double leftMotorSpeed = speed - speedDifference;
